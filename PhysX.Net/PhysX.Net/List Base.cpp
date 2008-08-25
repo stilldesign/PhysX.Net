f08d2388dde46bfff19eb2952296af2d34ad099e
Chad Mellor <chad@stilldesign.co.nz>
2008-08-25 03:40:47 +0000

    Working on List Base (Working version)

--- PhysX.Net/PhysX.Net/List Base.cpp
+++ PhysX.Net/PhysX.Net/List Base.cpp
@@ -2,3 +2,347 @@
 
 #include "List Base.h"
 
+using namespace StillDesign::PhysX;
+
+generic<class T>
+ListBase::ListBase()
+{
+	_collection = gcnew System::Collections::Generic::List<T>();
+}
+generic<class T>
+ListBase::ListBase( int capacity )
+{
+	_collection = gcnew System::Collections::Generic::List<T>( capacity );
+}
+generic<class T>
+ListBase::ListBase( System::Collections::Generic::IEnumerable<T>^ collection )
+{
+	_collection = gcnew System::Collections::Generic::List<T>( collection );
+}
+
+/// <summary>Adds a New Item to the Collection</summary>
+/// <param name="item">The Item to Add</param>
+generic<class T>
+void ListBase::Add( T item )
+{
+	_collection->Add( item );
+	
+	this->RaiseAdd( item );
+}
+generic<class T>
+int ListBase::Add( Object^ item ) = System::Collections::IList::Add
+{
+	int index = ((System::Collections::IList^)_collection)->Add( item );
+	
+	this->RaiseAdd( (T)item );
+	
+	return index;
+}
+void ListBase::AddRange( System::Collections::Generic::IEnumerable<T>^ collection )
+{
+	for each( T item in collection )
+	{
+		this->Add( item );
+	}
+}
+void ListBase::AddRange( array<T>^ collection )
+{
+	for each( T item in collection )
+	{
+		this->Add( item );
+	}
+}
+void ListBase::Insert( int index, T item )
+{
+	_collection->Insert( index, item );
+	
+	this->RaiseAdd( item );
+}
+void ListBase::Insert( int index, System::Collections::Generic::IEnumerable<T>^ collection )
+{
+	for each( T item in collection )
+	{
+		this->Insert( index++, item );
+	}
+}
+void ListBase::Insert( int index, Object^ item ) = System::Collections::IList::Insert
+{
+	((System::Collections::IList^)_collection)->Insert( index, item );
+}
+/// <summary>Removes an Item from the Collection</summary>
+/// <param name="item">The Item to Remove</param>
+/// <returns>True if the Item was Successfully Removed</returns>
+bool ListBase::Remove( T item )
+{
+	bool result = _collection->Remove( item );
+	
+	this->RaiseRemove( item );
+	
+	return result;
+}
+void ListBase::Remove( Object^ item ) = System::Collections::IList::Remove
+{
+	return ((System::Collections::IList^)_collection)->Remove( item );
+}
+void ListBase::RemoveAt( int index )
+{
+	T item = _collection[ index ];
+	
+	_collection->RemoveAt( index );
+	
+	this->RaiseRemove( item );
+}
+void ListBase::RemoveRange( int startIndex, int length )
+{
+	for( int x = 0; x < length; x++ )
+	{
+		this->RemoveAt( startIndex );
+	}
+}
+/// <summary>Removes All Items from the Collection</summary>
+void ListBase::Clear()
+{
+	int count = this->Count;
+	for( int x = 0; x < count; x++ )
+	{
+		if( this->Count > 0 )
+			this->RemoveAt( 0 );
+	}
+}
+	
+public:
+int ListBase::IndexOf( T item )
+{
+	return this->IndexOf( item, 0, this->Count );
+}
+int ListBase::IndexOf( T item, int startIndex )
+{
+	return this->IndexOf( item, startIndex, this->Count );
+}
+int ListBase::IndexOf( T item, int startIndex, int length )
+{
+	return _collection->IndexOf( item, startIndex, length );
+}
+int ListBase::IndexOf( Object^ item ) = System::Collections::IList::IndexOf
+{
+	return ((System::Collections::IList^)_collection)->IndexOf( item );
+}
+//virtual int IndexOf( Object^ item, int startIndex ) = System::Collections::IList::IndexOf
+//{
+//	return ((System::Collections::IList^)_collection)->IndexOf( item, startIndex );
+//}
+//virtual int IndexOf( Object^ item, int startIndex, int length ) = System::Collections::IList::IndexOf
+//{
+//	return ((System::Collections::IList^)_collection)->IndexOf( item, startIndex, length );
+//}
+bool ListBase::Contains( T item )
+{
+	return ( this->IndexOf( item ) != -1 );
+}
+bool ListBase::Contains( Object^ item ) = System::Collections::IList::Contains
+{
+	return ((System::Collections::IList^)_collection)->Contains( item );
+}
+bool ListBase::Exists( Predicate<T>^ match )
+{
+	return _collection->Exists( match );
+}
+T ListBase::Find( Predicate<T>^ match )
+{
+	return _collection->Find( match );
+}
+array<T>^ ListBase::FindAll( Predicate<T>^ match )
+{
+	return _collection->FindAll( match )->ToArray();
+}
+int ListBase::FindIndex( Predicate<T>^ match )
+{
+	return _collection->FindIndex( match );
+}
+int ListBase::FindIndex( Predicate<T>^ match, int startIndex )
+{
+	return _collection->FindIndex( startIndex, match );
+}
+int ListBase::FindIndex( Predicate<T>^ match, int startIndex, int length )
+{
+	return _collection->FindIndex( startIndex, length, match );
+}
+int ListBase::FindLastIndex( Predicate<T>^ match )
+{
+	return _collection->FindLastIndex( match );
+}
+int ListBase::FindLastIndex( Predicate<T>^ match, int startIndex )
+{
+	return _collection->FindLastIndex( startIndex, match );
+}
+int ListBase::FindLastIndex( Predicate<T>^ match, int startIndex, int length )
+{
+	return _collection->FindLastIndex( startIndex, length, match );
+}
+int ListBase::LastIndex( T item )
+{
+	return _collection->LastIndexOf( item );
+}
+int ListBase::LastIndexOf( T item, int startIndex )
+{
+	return _collection->LastIndexOf( item, startIndex );
+}
+int ListBase::LastIndexOf( T item, int startIndex, int length )
+{
+	return _collection->LastIndexOf( item, startIndex, length );
+}
+
+void ListBase::ForEach( Action<T>^ action )
+{
+	for each( T item in _collection )
+	{
+		action( item );
+	}
+}
+int ListBase::BinarySearch( T item )
+{
+	return _collection->BinarySearch( item );
+}
+int ListBase::BinarySearch( T item, System::Collections::Generic::IComparer<T>^ comparer )
+{
+	return _collection->BinarySearch( item, comparer );
+}
+int ListBase::BinarySearch( T item, System::Collections::Generic::IComparer<T>^ comparer, int index, int length )
+{
+	return _collection->BinarySearch( index, length, item, comparer );
+}
+bool ListBase::TrueForAll( Predicate<T>^ match )
+{
+	return _collection->TrueForAll( match );
+}
+
+void ListBase::CopyTo( Array^ a, int arrayIndex ) = System::Collections::ICollection::CopyTo
+{
+	((System::Collections::ICollection^)_collection)->CopyTo( a, arrayIndex );
+}
+/// <summary>Copies the Contents of this List to an Array</summary>
+/// <param name="array">The Array to Copy the Elements in to</param>
+/// <param name="arrayIndex">The index to use as an Offset</param>
+void ListBase::CopyTo( array<T>^ a, int arrayIndex )
+{
+	_collection->CopyTo( a, arrayIndex );
+}
+void ListBase::CopyTo( List<T>^ a )
+{
+	for each( T item in this )
+	{
+		a->Add( item );
+	}
+}
+array<T>^ ListBase::ToArray()
+{
+	return _collection->ToArray();
+}
+void ListBase::TrimExcess()
+{
+	_collection->TrimExcess();
+}
+System::Collections::ObjectModel::ReadOnlyCollection<T>^ ListBase::AsReadOnly()
+{
+	return _collection->AsReadOnly();
+}
+generic<class TOutput>
+System::Collections::Generic::List<TOutput>^ ListBase::ConvertAll( Converter<T, TOutput>^ converter )
+{
+	return _collection->ConvertAll<TOutput>( converter );
+}
+System::Collections::Generic::List<T>^ ListBase::GetRange( int startIndex, int length )
+{
+	return _collection->GetRange( startIndex, length );
+}
+void ListBase::Reverse()
+{
+	_collection->Reverse();
+}
+void ListBase::Reverse( int startIndex, int length )
+{
+	_collection->Reverse( startIndex, length );
+}
+void ListBase::Sort()
+{
+	_collection->Sort();
+}
+void ListBase::Sort( Comparison<T>^ comparison )
+{
+	_collection->Sort( comparison );
+}
+void ListBase::Sort( System::Collections::Generic::IComparer<T>^ comparer, int startIndex, int length )
+{
+	_collection->Sort( startIndex, length, comparer );
+}
+void ListBase::Sort( System::Collections::Generic::IComparer<T>^ comparer )
+{
+	_collection->Sort( comparer );
+}
+
+System::Collections::IEnumerator^ ListBase::GetEnumerator1() = System::Collections::IEnumerable::GetEnumerator
+{
+	return ((System::Collections::IList^)_collection)->GetEnumerator();
+}
+System::Collections::Generic::IEnumerator<T>^ ListBase::GetEnumerator2() = System::Collections::Generic::IEnumerable<T>::GetEnumerator
+{
+	return ((System::Collections::Generic::IList<T>^)_collection)->GetEnumerator();
+}
+
+#pragma region Properties
+	/// <summary>Gets the Count of Items in the Collection</summary>
+	int ListBase::Count::get()
+	{
+		return _collection->Count;
+	}
+	int ListBase::Capacity::get()
+	{
+		return _collection->Capacity;
+	}
+	
+	property bool ListBase::IsReadOnly::get()
+	{
+		return false;
+	}
+	property bool ListBase::IsFixedSize::get()
+	{
+		return ((System::Collections::IList^)_collection)->IsFixedSize;
+	}
+	
+	property bool ListBase::IsSynchronized::get() = System::Collections::ICollection::IsSynchronized::get
+	{
+		return ((System::Collections::ICollection^)_collection)->IsSynchronized;
+	}
+	property Object^ ListBase::SyncRoot::get()
+	{
+		return ((System::Collections::ICollection^)_collection)->SyncRoot;
+	}
+	
+	property Object^ ListBase::default1::get( int index ) = System::Collections::IList::default::get
+	{
+		return _collection[ index ];
+	}
+	virtual void ListBase::default1::set( int index, Object^ value ) = System::Collections::IList::default::set
+	{
+		this->RaiseRemove( (T)_collection[ index ] );
+		
+		_collection[ index ] = (T)value;
+		
+		this->RaiseAdd( (T)_collection[ index ] );
+	}
+	/// <summary>Gets or Sets the Item at The Index</summary>
+	/// <param name="index">Index to Access</param>
+	/// <returns>An Object of T</returns>
+	property T ListBase::default::get( int index ) = System::Collections::Generic::IList<T>::default::get
+	{
+		return _collection[ index ];
+	}
+	void ListBase::default::set( int index, T value ) = System::Collections::Generic::IList<T>::default::set
+	{
+		this->RaiseRemove( _collection[ index ] );
+		
+		_collection[ index ] = value;
+		
+		this->RaiseAdd( _collection[ index ] );
+	}
+#pragma endregion
--- PhysX.Net/PhysX.Net/List Base.h
+++ PhysX.Net/PhysX.Net/List Base.h
@@ -40,365 +40,120 @@ namespace StillDesign
 				}
 			
 			public:
-				ListBase()
-				{
-					_collection = gcnew System::Collections::Generic::List<T>();
-				}
-				ListBase( int capacity )
-				{
-					_collection = gcnew System::Collections::Generic::List<T>( capacity );
-				}
-				ListBase( System::Collections::Generic::IEnumerable<T>^ collection )
-				{
-					_collection = gcnew System::Collections::Generic::List<T>( collection );
-				}
-
+				ListBase();
+				ListBase( int capacity );
+				ListBase( System::Collections::Generic::IEnumerable<T>^ collection );
+				
 				/// <summary>Adds a New Item to the Collection</summary>
 				/// <param name="item">The Item to Add</param>
-				virtual void Add( T item )
-				{
-					_collection->Add( item );
-					
-					this->RaiseAdd( item );
-				}
-				virtual int Add( Object^ item ) = System::Collections::IList::Add
-				{
-					int index = ((System::Collections::IList^)_collection)->Add( item );
-					
-					this->RaiseAdd( (T)item );
-					
-					return index;
-				}
-				virtual void AddRange( System::Collections::Generic::IEnumerable<T>^ collection )
-				{
-					for each( T item in collection )
-					{
-						this->Add( item );
-					}
-				}
-				virtual void AddRange( array<T>^ collection )
-				{
-					for each( T item in collection )
-					{
-						this->Add( item );
-					}
-				}
-				virtual void Insert( int index, T item )
-				{
-					_collection->Insert( index, item );
-					
-					this->RaiseAdd( item );
-				}
-				void Insert( int index, System::Collections::Generic::IEnumerable<T>^ collection )
-				{
-					for each( T item in collection )
-					{
-						this->Insert( index++, item );
-					}
-				}
-				virtual void Insert( int index, Object^ item ) = System::Collections::IList::Insert
-				{
-					((System::Collections::IList^)_collection)->Insert( index, item );
-				}
+				virtual void Add( T item );
+				virtual int Add( Object^ item ) = System::Collections::IList::Add;
+				virtual void AddRange( System::Collections::Generic::IEnumerable<T>^ collection );
+				virtual void AddRange( array<T>^ collection );
+				virtual void Insert( int index, T item );
+				void Insert( int index, System::Collections::Generic::IEnumerable<T>^ collection );
+				virtual void Insert( int index, Object^ item ) = System::Collections::IList::Insert;
 				/// <summary>Removes an Item from the Collection</summary>
 				/// <param name="item">The Item to Remove</param>
 				/// <returns>True if the Item was Successfully Removed</returns>
-				virtual bool Remove( T item )
-				{
-					bool result = _collection->Remove( item );
-					
-					this->RaiseRemove( item );
-					
-					return result;
-				}
-				virtual void Remove( Object^ item ) = System::Collections::IList::Remove
-				{
-					return ((System::Collections::IList^)_collection)->Remove( item );
-				}
-				virtual void RemoveAt( int index )
-				{
-					T item = _collection[ index ];
-					
-					_collection->RemoveAt( index );
-					
-					this->RaiseRemove( item );
-				}
-				virtual void RemoveRange( int startIndex, int length )
-				{
-					for( int x = 0; x < length; x++ )
-					{
-						this->RemoveAt( startIndex );
-					}
-				}
+				virtual bool Remove( T item );
+				virtual void Remove( Object^ item ) = System::Collections::IList::Remove;
+				virtual void RemoveAt( int index );
+				virtual void RemoveRange( int startIndex, int length );
 				/// <summary>Removes All Items from the Collection</summary>
-				virtual void Clear()
-				{
-					int count = this->Count;
-					for( int x = 0; x < count; x++ )
-					{
-						if( this->Count > 0 )
-							this->RemoveAt( 0 );
-					}
-				}
+				virtual void Clear();
 					
 			public:
-				virtual int IndexOf( T item )
-				{
-					return this->IndexOf( item, 0, this->Count );
-				}
-				virtual int IndexOf( T item, int startIndex )
-				{
-					return this->IndexOf( item, startIndex, this->Count );
-				}
-				virtual int IndexOf( T item, int startIndex, int length )
-				{
-					return _collection->IndexOf( item, startIndex, length );
-				}
-				virtual int IndexOf( Object^ item ) = System::Collections::IList::IndexOf
-				{
-					return ((System::Collections::IList^)_collection)->IndexOf( item );
-				}
-				//virtual int IndexOf( Object^ item, int startIndex ) = System::Collections::IList::IndexOf
-				//{
-				//	return ((System::Collections::IList^)_collection)->IndexOf( item, startIndex );
-				//}
-				//virtual int IndexOf( Object^ item, int startIndex, int length ) = System::Collections::IList::IndexOf
-				//{
-				//	return ((System::Collections::IList^)_collection)->IndexOf( item, startIndex, length );
-				//}
-				virtual bool Contains( T item )
-				{
-					return ( this->IndexOf( item ) != -1 );
-				}
-				virtual bool Contains( Object^ item ) = System::Collections::IList::Contains
-				{
-					return ((System::Collections::IList^)_collection)->Contains( item );
-				}
-				virtual bool Exists( Predicate<T>^ match )
-				{
-					return _collection->Exists( match );
-				}
-				virtual T Find( Predicate<T>^ match )
-				{
-					return _collection->Find( match );
-				}
-				virtual array<T>^ FindAll( Predicate<T>^ match )
-				{
-					return _collection->FindAll( match )->ToArray();
-				}
-				virtual int FindIndex( Predicate<T>^ match )
-				{
-					return _collection->FindIndex( match );
-				}
-				virtual int FindIndex( Predicate<T>^ match, int startIndex )
-				{
-					return _collection->FindIndex( startIndex, match );
-				}
-				virtual int FindIndex( Predicate<T>^ match, int startIndex, int length )
-				{
-					return _collection->FindIndex( startIndex, length, match );
-				}
-				virtual int FindLastIndex( Predicate<T>^ match )
-				{
-					return _collection->FindLastIndex( match );
-				}
-				virtual int FindLastIndex( Predicate<T>^ match, int startIndex )
-				{
-					return _collection->FindLastIndex( startIndex, match );
-				}
-				virtual int FindLastIndex( Predicate<T>^ match, int startIndex, int length )
-				{
-					return _collection->FindLastIndex( startIndex, length, match );
-				}
-				virtual int LastIndex( T item )
-				{
-					return _collection->LastIndexOf( item );
-				}
-				virtual int LastIndexOf( T item, int startIndex )
-				{
-					return _collection->LastIndexOf( item, startIndex );
-				}
-				virtual int LastIndexOf( T item, int startIndex, int length )
-				{
-					return _collection->LastIndexOf( item, startIndex, length );
-				}
-
-				virtual void ForEach( Action<T>^ action )
-				{
-					for each( T item in _collection )
-					{
-						action( item );
-					}
-				}
-				virtual int BinarySearch( T item )
-				{
-					return _collection->BinarySearch( item );
-				}
-				virtual int BinarySearch( T item, System::Collections::Generic::IComparer<T>^ comparer )
-				{
-					return _collection->BinarySearch( item, comparer );
-				}
-				virtual int BinarySearch( T item, System::Collections::Generic::IComparer<T>^ comparer, int index, int length )
-				{
-					return _collection->BinarySearch( index, length, item, comparer );
-				}
-				bool TrueForAll( Predicate<T>^ match )
-				{
-					return _collection->TrueForAll( match );
-				}
+				virtual int IndexOf( T item );
+				virtual int IndexOf( T item, int startIndex );
+				virtual int IndexOf( T item, int startIndex, int length );
+				virtual int IndexOf( Object^ item ) = System::Collections::IList::IndexOf;
 				
-				virtual void CopyTo( Array^ a, int arrayIndex ) = System::Collections::ICollection::CopyTo
-				{
-					((System::Collections::ICollection^)_collection)->CopyTo( a, arrayIndex );
-				}
+				virtual bool Contains( T item );
+				virtual bool Contains( Object^ item ) = System::Collections::IList::Contains;
+				virtual bool Exists( Predicate<T>^ match );
+				virtual T Find( Predicate<T>^ match );
+				virtual array<T>^ FindAll( Predicate<T>^ match );
+
+				virtual int FindIndex( Predicate<T>^ match );
+				virtual int FindIndex( Predicate<T>^ match, int startIndex );
+				virtual int FindIndex( Predicate<T>^ match, int startIndex, int length );
+				virtual int FindLastIndex( Predicate<T>^ match );
+				virtual int FindLastIndex( Predicate<T>^ match, int startIndex );
+				virtual int FindLastIndex( Predicate<T>^ match, int startIndex, int length );
+				virtual int LastIndex( T item );
+				virtual int LastIndexOf( T item, int startIndex );
+				virtual int LastIndexOf( T item, int startIndex, int length );
+
+				virtual void ForEach( Action<T>^ action );
+				virtual int BinarySearch( T item );
+				virtual int BinarySearch( T item, System::Collections::Generic::IComparer<T>^ comparer );
+				virtual int BinarySearch( T item, System::Collections::Generic::IComparer<T>^ comparer, int index, int length );
+				bool TrueForAll( Predicate<T>^ match );
+				
+				virtual void CopyTo( Array^ a, int arrayIndex ) = System::Collections::ICollection::CopyTo;
 				/// <summary>Copies the Contents of this List to an Array</summary>
 				/// <param name="array">The Array to Copy the Elements in to</param>
 				/// <param name="arrayIndex">The index to use as an Offset</param>
-				virtual void CopyTo( array<T>^ a, int arrayIndex )
-				{
-					_collection->CopyTo( a, arrayIndex );
-				}
-				virtual void CopyTo( List<T>^ a )
-				{
-					for each( T item in this )
-					{
-						a->Add( item );
-					}
-				}
-				virtual array<T>^ ToArray()
-				{
-					return _collection->ToArray();
-				}
-				virtual void TrimExcess()
-				{
-					_collection->TrimExcess();
-				}
-				virtual System::Collections::ObjectModel::ReadOnlyCollection<T>^ AsReadOnly()
-				{
-					return _collection->AsReadOnly();
-				}
+				virtual void CopyTo( array<T>^ a, int arrayIndex );
+				virtual void CopyTo( List<T>^ a );
+				virtual array<T>^ ToArray();
+				virtual void TrimExcess();
+				virtual System::Collections::ObjectModel::ReadOnlyCollection<T>^ AsReadOnly();
 				generic<class TOutput>
-				virtual System::Collections::Generic::List<TOutput>^ ConvertAll( Converter<T, TOutput>^ converter )
-				{
-					return _collection->ConvertAll<TOutput>( converter );
-				}
-				virtual System::Collections::Generic::List<T>^ GetRange( int startIndex, int length )
-				{
-					return _collection->GetRange( startIndex, length );
-				}
-				virtual void Reverse()
-				{
-					_collection->Reverse();
-				}
-				virtual void Reverse( int startIndex, int length )
-				{
-					_collection->Reverse( startIndex, length );
-				}
-				virtual void Sort()
-				{
-					_collection->Sort();
-				}
-				virtual void Sort( Comparison<T>^ comparison )
-				{
-					_collection->Sort( comparison );
-				}
-				virtual void Sort( System::Collections::Generic::IComparer<T>^ comparer, int startIndex, int length )
-				{
-					_collection->Sort( startIndex, length, comparer );
-				}
-				virtual void Sort( System::Collections::Generic::IComparer<T>^ comparer )
-				{
-					_collection->Sort( comparer );
-				}
+				virtual System::Collections::Generic::List<TOutput>^ ConvertAll( Converter<T, TOutput>^ converter );
+				virtual System::Collections::Generic::List<T>^ GetRange( int startIndex, int length );
+				virtual void Reverse();
+				virtual void Reverse( int startIndex, int length );
+				virtual void Sort();
+				virtual void Sort( Comparison<T>^ comparison );
+				virtual void Sort( System::Collections::Generic::IComparer<T>^ comparer, int startIndex, int length );
+				virtual void Sort( System::Collections::Generic::IComparer<T>^ comparer );
 				
-				virtual System::Collections::IEnumerator^ GetEnumerator1() = System::Collections::IEnumerable::GetEnumerator
-				{
-					return ((System::Collections::IList^)_collection)->GetEnumerator();
-				}
-				virtual System::Collections::Generic::IEnumerator<T>^ GetEnumerator2() = System::Collections::Generic::IEnumerable<T>::GetEnumerator
-				{
-					return ((System::Collections::Generic::IList<T>^)_collection)->GetEnumerator();
-				}
+				virtual System::Collections::IEnumerator^ GetEnumerator1() = System::Collections::IEnumerable::GetEnumerator;
+				virtual System::Collections::Generic::IEnumerator<T>^ GetEnumerator2() = System::Collections::Generic::IEnumerable<T>::GetEnumerator;
 				
 				#pragma region Properties
 					/// <summary>Gets the Count of Items in the Collection</summary>
 					property int Count
 					{
-						virtual int get()
-						{
-							return _collection->Count;
-						}
+						virtual int get();
 					}
 					property int Capacity
 					{
-						virtual int get()
-						{
-							return _collection->Capacity;
-						}
+						virtual int get();
 					}
 
 					property bool IsReadOnly
 					{
-						virtual bool get()
-						{
-							return false;
-						}
+						virtual bool get();
 					}
 					property bool IsFixedSize
 					{
-						virtual bool get()
-						{
-							return ((System::Collections::IList^)_collection)->IsFixedSize;
-						}
+						virtual bool get();
 					}
 
 					property bool IsSynchronized
 					{
-						virtual bool get() = System::Collections::ICollection::IsSynchronized::get
-						{
-							return ((System::Collections::ICollection^)_collection)->IsSynchronized;
-						}
+						virtual bool get() = System::Collections::ICollection::IsSynchronized::get;
 					}
 					property Object^ SyncRoot
 					{
-						virtual Object^ get()
-						{
-							return ((System::Collections::ICollection^)_collection)->SyncRoot;
-						}
+						virtual Object^ get();
 					}
 					
 					property Object^ default1[ int ]
 					{
-						virtual Object^ get( int index ) = System::Collections::IList::default::get
-						{
-							return _collection[ index ];
-						}
-						virtual void set( int index, Object^ value ) = System::Collections::IList::default::set
-						{
-							this->RaiseRemove( (T)_collection[ index ] );
-							
-							_collection[ index ] = (T)value;
-							
-							this->RaiseAdd( (T)_collection[ index ] );
-						}
+						virtual Object^ get( int index ) = System::Collections::IList::default::get;
+						virtual void set( int index, Object^ value ) = System::Collections::IList::default::set;
 					}
 					/// <summary>Gets or Sets the Item at The Index</summary>
 					/// <param name="index">Index to Access</param>
 					/// <returns>An Object of T</returns>
 					property T default[ int ]
 					{
-						virtual T get( int index ) = System::Collections::Generic::IList<T>::default::get
-						{
-							return _collection[ index ];
-						}
-						virtual void set( int index, T value ) = System::Collections::Generic::IList<T>::default::set
-						{
-							this->RaiseRemove( _collection[ index ] );
-							
-							_collection[ index ] = value;
-							
-							this->RaiseAdd( _collection[ index ] );
-						}
+						virtual T get( int index ) = System::Collections::Generic::IList<T>::default::get;
+						virtual void set( int index, T value ) = System::Collections::Generic::IList<T>::default::set;
 					}
 		#pragma endregion
 		};
