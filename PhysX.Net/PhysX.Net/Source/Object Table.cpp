a83c7d616f7efb3658e33bc1152510f1dfa92442
Chad Mellor <chad@stilldesign.co.nz>
2009-03-23 03:27:40 +0000

    ObjectTable is now sealed Added ownership management to ObjectTable

--- PhysX.Net/PhysX.Net/Source/Object Table.cpp
+++ PhysX.Net/PhysX.Net/Source/Object Table.cpp
@@ -3,88 +3,194 @@
 #include "Object Table.h"
 #include "IDisposable.h"
 
+using namespace System::Threading;
 using namespace System::Collections::Generic;
+
 using namespace StillDesign::PhysX;
 
 void StillDesign::PhysX::ObjectTable::Add( intptr_t pointer, Object^ object )
 {
-	if( IsInstanceOf<StillDesign::PhysX::IDisposable^>( object ) == true )
-	{
-		StillDesign::PhysX::IDisposable^ disposableObject = dynamic_cast<StillDesign::PhysX::IDisposable^>( object );
-		
-		disposableObject->OnDisposing += gcnew EventHandler( &ObjectTable::_cache_OnDisposing );
-	}
+	Add( pointer, object, nullptr );
+}
+void StillDesign::PhysX::ObjectTable::Add( intptr_t pointer, Object^ object, Object^ owner )
+{
+	if( pointer == NULL )
+		throw gcnew PhysXException( "Invalid pointer added to Object Table", "pointer" );
+	if( object == nullptr )
+		throw gcnew ArgumentNullException( "Invalid pointer added to Object Table", "pointer" );
 	
+	Monitor::Enter( _syncObject );
 	try
 	{
-		_cache->Add( pointer, object );
-	}catch( Exception^ ex ){
-		throw;
+		if( IsInstanceOf<StillDesign::PhysX::IDisposable^>( object ) == true )
+		{
+			StillDesign::PhysX::IDisposable^ disposableObject = dynamic_cast<StillDesign::PhysX::IDisposable^>( object );
+			
+			disposableObject->OnDisposing += gcnew EventHandler( &ObjectTable::_objectTable_OnDisposing );
+		}
+		
+		try
+		{
+			_objectTable->Add( pointer, object );
+			
+			if( owner != nullptr )
+				AddOwnership( object, owner );
+			
+			ObjectAdded( nullptr, gcnew ObjectTableEventArgs( pointer, object ) );
+		}catch( Exception^ ex ){
+			throw;
+		}
+	}finally{
+		Monitor::Exit( _syncObject );
 	}
 }
 
 bool StillDesign::PhysX::ObjectTable::Remove( intptr_t pointer )
 {
-	Object^ object = _cache[ pointer ];
-	
-	if( IsInstanceOf<StillDesign::PhysX::IDisposable^>( object ) == true )
+	Monitor::Enter( _syncObject );
+	try
 	{
-		StillDesign::PhysX::IDisposable^ disposableObject = dynamic_cast<StillDesign::PhysX::IDisposable^>( object );
+		Object^ object = _objectTable[ pointer ];
 		
-		disposableObject->OnDisposing -= gcnew EventHandler( &ObjectTable::_cache_OnDisposing );
+		if( IsInstanceOf<StillDesign::PhysX::IDisposable^>( object ) == true )
+		{
+			StillDesign::PhysX::IDisposable^ disposableObject = dynamic_cast<StillDesign::PhysX::IDisposable^>( object );
+			
+			disposableObject->OnDisposing -= gcnew EventHandler( &ObjectTable::_objectTable_OnDisposing );
+		}
+		
+		bool result = _objectTable->Remove( pointer );
+		
+		if( _ownership->ContainsKey( object ) == true )
+		{
+			for each( Object^ ownershipObject in _ownership[ object ] )
+			{
+				_objectTable->Remove( GetObject( ownershipObject ) );
+			}
+			_ownership->Remove( object );
+		}
+		
+		ObjectRemoved( nullptr, gcnew ObjectTableEventArgs( pointer, object ) );
+		
+		return result;
+	}finally{
+		Monitor::Exit( _syncObject );
 	}
-	
-	return _cache->Remove( pointer );
 }
 bool StillDesign::PhysX::ObjectTable::Remove( Object^ object )
 {
-	for each( KeyValuePair<intptr_t, Object^>^ pair in _cache )
+	Monitor::Enter( _syncObject );
+	try
 	{
-		if( pair->Value == object )
-			return Remove( pair->Key );
+		for each( KeyValuePair<intptr_t, Object^>^ pair in _objectTable )
+		{
+			if( pair->Value == object )
+				return Remove( pair->Key );
+		}
+		
+		return false;
+	}finally{
+		Monitor::Exit( _syncObject );
 	}
-	
-	return false;
 }
 
 void StillDesign::PhysX::ObjectTable::Clear()
 {
-	_cache->Clear();
+	Monitor::Enter( _syncObject );
+	try
+	{
+		_objectTable->Clear();
+	}finally{
+		Monitor::Exit( _syncObject );
+	}
 }
 
 generic<class T>
 T StillDesign::PhysX::ObjectTable::GetObject( intptr_t pointer )
 {
-	return (T)_cache[ pointer ];
+	Monitor::Enter( _syncObject );
+	try
+	{
+		return (T)_objectTable[ pointer ];
+	}finally{
+		Monitor::Exit( _syncObject );
+	}
+}
+intptr_t StillDesign::PhysX::ObjectTable::GetObject( Object^ object )
+{
+	Monitor::Enter( _syncObject );
+	try
+	{
+		for each( KeyValuePair<intptr_t, Object^>^ pair in _objectTable )
+		{
+			if( pair->Value == object )
+			{
+				return pair->Key;
+			}
+		}
+		
+		throw gcnew Exception( "Unable to find object" );
+	}finally{
+		Monitor::Exit( _syncObject );
+	}
 }
 
 bool StillDesign::PhysX::ObjectTable::Contains( intptr_t pointer )
 {
-	return _cache->ContainsKey( pointer );
+	Monitor::Enter( _syncObject );
+	try
+	{
+		return _objectTable->ContainsKey( pointer );
+	}finally{
+		Monitor::Exit( _syncObject );
+	}
+}
+
+void StillDesign::PhysX::ObjectTable::AddOwnership( Object^ object, Object^ owner )
+{
+	if( object == nullptr )
+		throw gcnew ArgumentNullException( "object" );
+	if( owner == nullptr )
+		throw gcnew ArgumentNullException( "owner" );
+	
+	//Monitor::Enter( _syncObject );
+	//try
+	//{
+	
+	if( _ownership->ContainsKey( owner ) == false )
+	{
+		_ownership->Add( owner, gcnew OwnershipObjects() );
+	}
+	
+	_ownership[ owner ]->Add( object );
+	
+	//}finally{
+		//Monitor::Exit( _syncObject );
+	//}
 }
 
 //generic<class T>
 //T ObjectTable::GetObjectByIndexIfType( int index )
 //{
-//	intptr_t key = _cache->Keys[ index ];
+//	intptr_t key = _objectTable->Keys[ index ];
 //	
-//	for each( intptr_t key in _cache->Keys )
+//	for each( intptr_t key in _objectTable->Keys )
 //	{
 //		
 //	}
 //	
-//	return dynamic_cast<T>( _cache[ key ] );
+//	return dynamic_cast<T>( _objectTable[ key ] );
 //}
 //generic<class T>
 //array<T>^ ObjectTable::GetObjectsOfType()
 //{
 //	System::Collections::Generic::List<T>^ items = gcnew System::Collections::Generic::List<T>();
 //	
-//	for each( intptr_t key in _cache->Keys )
+//	for each( intptr_t key in _objectTable->Keys )
 //	{
-//		if( dynamic_cast<T>( _cache[ key ] ) != nullptr )
+//		if( dynamic_cast<T>( _objectTable[ key ] ) != nullptr )
 //		{
-//			items->Add( _cache[ key ] );
+//			items->Add( _objectTable[ key ] );
 //		}
 //	}
 //	
@@ -99,7 +205,7 @@ bool StillDesign::PhysX::ObjectTable::Contains( intptr_t pointer )
 //	
 //	Object^ obj;
 //	
-//	bool b = _cache->TryGetValue( pointer, obj );
+//	bool b = _objectTable->TryGetValue( pointer, obj );
 //	
 //	if( b == true )
 //		return (T)obj;
@@ -109,10 +215,22 @@ bool StillDesign::PhysX::ObjectTable::Contains( intptr_t pointer )
 
 int StillDesign::PhysX::ObjectTable::Count::get()
 {
-	return _cache->Count;
+	Monitor::Enter( _syncObject );
+	try
+	{
+		return _objectTable->Count;
+	}finally{
+		Monitor::Exit( _syncObject );
+	}
 }
 
-void StillDesign::PhysX::ObjectTable::_cache_OnDisposing( Object^ sender, EventArgs^ e )
+void StillDesign::PhysX::ObjectTable::_objectTable_OnDisposing( Object^ sender, EventArgs^ e )
 {
-	Remove( sender );
+	Monitor::Enter( _syncObject );
+	try
+	{
+		Remove( sender );
+	}finally{
+		Monitor::Exit( _syncObject );
+	}
 }
--- PhysX.Net/PhysX.Net/Source/Object Table.h
+++ PhysX.Net/PhysX.Net/Source/Object Table.h
@@ -4,19 +4,33 @@ namespace StillDesign
 {
 	namespace PhysX
 	{
-		private ref class ObjectTable
+		ref class OwnershipObjects;
+		ref class ObjectTableEventArgs;
+		
+		private ref class ObjectTable sealed
 		{
+			public:
+                static event System::EventHandler<ObjectTableEventArgs^>^ ObjectAdded;
+                static event System::EventHandler<ObjectTableEventArgs^>^ ObjectRemoved;
+			
 			private:
-				static System::Collections::Generic::Dictionary<intptr_t, Object^>^ _cache;
+				static System::Collections::Generic::Dictionary<intptr_t, Object^>^ _objectTable;
+				static System::Collections::Generic::Dictionary<Object^, OwnershipObjects^>^ _ownership;
+				
+				static Object^ _syncObject;
 				
 			private:
 				static ObjectTable()
 				{
-					_cache = gcnew System::Collections::Generic::Dictionary<intptr_t, Object^>();
+					_objectTable = gcnew System::Collections::Generic::Dictionary<intptr_t, Object^>();
+					_ownership = gcnew System::Collections::Generic::Dictionary<Object^, OwnershipObjects^>();
+					
+					_syncObject = gcnew Object();
 				}
 				
 			public:
 				static void Add( intptr_t pointer, Object^ object );
+				static void Add( intptr_t pointer, Object^ object, Object^ owner );
 				
 				static bool Remove( intptr_t pointer );
 				static bool Remove( Object^ object );
@@ -24,8 +38,9 @@ namespace StillDesign
 				static void Clear();
 				
 				generic<class T>
-				//where T : ref class
 				static T GetObject( intptr_t pointer );
+				static intptr_t GetObject( Object^ object );
+				
 				//generic<class T>
 				//where T : ref class
 				//static T GetObjectOrNull( intptr_t pointer );
@@ -38,22 +53,42 @@ namespace StillDesign
 				
 				static bool Contains( intptr_t pointer );
 				
+				static void AddOwnership( Object^ object, Object^ owner );
+				
 				property int Count
 				{
 					static int get();
 				}
 				
 			private:
-				static void _cache_OnDisposing( Object^ sender, EventArgs^ e );
+				static void _objectTable_OnDisposing( Object^ sender, EventArgs^ e );
 				
 			internal:
 				property System::Collections::Generic::Dictionary<intptr_t, Object^>^ Cache
 				{
 					static System::Collections::Generic::Dictionary<intptr_t, Object^>^ get()
 					{
-						return _cache;
+						return _objectTable;
 					}
 				}
+		};
+		
+		public ref class OwnershipObjects : System::Collections::Generic::List<Object^>
+		{
+			
+		};
+		
+		public ref class ObjectTableEventArgs : System::EventArgs
+		{
+			public:
+				ObjectTableEventArgs( intptr_t unmanagedObject, Object^ managedObject )
+				{
+					this->UnmanagedObject = unmanagedObject;
+					this->ManagedObject = managedObject;
+				}
+				
+				property intptr_t UnmanagedObject;
+				property Object^ ManagedObject;
 		};
 	};
 };
