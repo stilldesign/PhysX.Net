68e244e8c564c65bde8d327eee3ba92b42c806d2
Chad Mellor <chad@stilldesign.co.nz>
2011-03-19 02:54:57 +0000

    Cleaned up sample code slightly Updated Changelog Removed old files

--- PhysX.Net/Samples/Tearing Cloth/TearingCloth.cs
+++ PhysX.Net/Samples/Tearing Cloth/TearingCloth.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using SlimDX;
+using StillDesign.PhysX.MathPrimitives;
 
 namespace StillDesign.PhysX.Samples
 {
@@ -22,7 +22,7 @@ namespace StillDesign.PhysX.Samples
 
 		protected override void LoadContent()
 		{
-			Engine.Camera.View = Matrix.LookAtLH(new Vector3(0, 30, -40), new Vector3(0, 20, 0), new Vector3(0, 1, 0));
+			Engine.Camera.View = SlimDX.Matrix.LookAtLH(new SlimDX.Vector3(0, 30, -40), new SlimDX.Vector3(0, 20, 0), new SlimDX.Vector3(0, 1, 0));
 		}
 
 		protected override void Update(TimeSpan elapsed)
@@ -32,34 +32,152 @@ namespace StillDesign.PhysX.Samples
 
 		protected override void Draw()
 		{
-			//int n = _cloth.MeshData.NumberOfVertices.Value;
+			
+		}
 
-			//var positions = _cloth.MeshData.PositionsStream.GetData<Vector3>();
-			//var normals = _cloth.MeshData.NormalsStream.GetData<Vector3>();
-			//var indicies = _cloth.MeshData.IndicesStream.GetData<int>();
+		protected override void LoadPhysics(Scene scene)
+		{
+			int w = 25;
+			int h = 25;
 
-			//ClothVertex[] vertices = new ClothVertex[ n ];
+			float hw = w / 2.0f;
+			float hh = h / 2.0f;
 
-			//for( int x = 0; x < n; x++ )
-			//{
-			//    vertices[ x ].Position = positions[ x ];
-			//    vertices[ x ].Normal = normals[ x ];
-			//}
+			Vector3 p = new Vector3(0, 20, 0);
 
-			//_lighting.TextureEnabled = false;
-			//_lighting.World = Matrix.Identity;
-			//_lighting.View = _engine.Camera.View;
-			//_lighting.Projection = _engine.Camera.Projection;
-			//_lighting.LightingEnabled = false;
+			// Create a Grid of Points
+			int vertices, indices;
 
-			//_lighting.Begin();
-			//foreach( var pass in _lighting.CurrentTechnique.Passes )
-			//{
-			//    pass.Begin();
-			//    _engine.Device.DrawUserIndexedPrimitives<ClothVertex>( PrimitiveType.TriangleList, vertices, 0, n, indicies, 0, indicies.Length / 3 );
-			//    pass.End();
-			//}
-			//_lighting.End();
+			ClothMesh clothMesh;
+			{
+				var grid = VertexGrid.CreateGrid(w, h);
+
+				vertices = grid.Points.Length;
+				indices = grid.Indices.Length;
+
+				ClothMeshDescription clothMeshDesc = new ClothMeshDescription();
+				clothMeshDesc.AllocateVertices<Vector3>(vertices);
+				clothMeshDesc.AllocateTriangles<int>(indices / 3);
+
+				clothMeshDesc.VertexCount = vertices;
+				clothMeshDesc.TriangleCount = indices / 3;
+
+				clothMeshDesc.VerticesStream.SetData(grid.Points);
+				clothMeshDesc.TriangleStream.SetData(grid.Indices);
+
+				// We are using 32 bit integers for our indices, so make sure the 16 bit flag is removed.
+				// 32 bits are the default, so this isn't technically needed, but it's good to show in a sample
+				clothMeshDesc.Flags &= ~MeshFlag.Indices16Bit;
+				clothMeshDesc.Flags |= (MeshFlag)((int)clothMeshDesc.Flags | (int)ClothMeshFlag.Tearable);
+
+				// Write the cooked data to memory
+				using (var memoryStream = new MemoryStream())
+				{
+					Cooking.InitializeCooking();
+					Cooking.CookClothMesh(clothMeshDesc, memoryStream);
+					Cooking.CloseCooking();
+
+					// Need to reset the position of the stream to the beginning
+					memoryStream.Position = 0;
+
+					clothMesh = Engine.Core.CreateClothMesh(memoryStream);
+				}
+			}
+
+			//
+
+			int j = vertices * 2;
+			int k = indices * 3;
+
+			var clothDesc = new ClothDescription()
+			{
+				ClothMesh = clothMesh,
+				GlobalPose =
+					Matrix.Translation(-hw, 0, -hh) *
+					Matrix.Translation(p),
+				Flags = ClothFlag.Gravity | ClothFlag.Bending | ClothFlag.CollisionTwoway | ClothFlag.Visualization | ClothFlag.Tearable,
+				BendingStiffness = 0.1f,
+				TearFactor = 1.5f
+			};
+			clothDesc.MeshData.AllocatePositions<Vector3>(j);
+			clothDesc.MeshData.AllocateIndices<int>(k);
+			clothDesc.MeshData.AllocateNormals<Vector3>(j);
+
+			clothDesc.MeshData.MaximumVertices = j;
+			clothDesc.MeshData.MaximumIndices = k;
+
+			clothDesc.MeshData.NumberOfVertices = vertices;
+			clothDesc.MeshData.NumberOfIndices = indices;
+
+			_cloth = scene.CreateCloth(clothDesc);
+
+			//
+
+			// Four corner boxes to hold it in place
+			var positions = new[]
+			{
+				new Vector3(0, 0, -hh), // Back
+				new Vector3(0, 0, hh), // Front
+				new Vector3(-hw, 0, 0), // Left
+				new Vector3(hw, 0, 0), // Right
+			};
+
+			var sizes = new[]
+			{
+				new Vector3(w, 1, 1), // Back
+				new Vector3(w, 1, 1), // Front
+				new Vector3(1, 1, h), // Left
+				new Vector3(1, 1, h), //Right
+			};
+
+			for (int i = 0; i < 4; i++)
+			{
+				var actorDesc = new ActorDescription()
+				{
+					GlobalPose = Matrix.Translation(positions[i] + p),
+					Shapes = { new BoxShapeDescription(sizes[i]) }
+				};
+
+				var actor = scene.CreateActor(actorDesc);
+
+				_cloth.AttachToShape(actor.Shapes.First(), (ClothAttachmentFlag)0);
+			}
+
+			//
+
+			// Something to drop on it
+			{
+				var actorDesc = new ActorDescription()
+				{
+					GlobalPose = Matrix.Translation(0, 100, 0),
+					Shapes = { new SphereShapeDescription(2) },
+					BodyDescription = new BodyDescription(50)
+				};
+
+				var actor = scene.CreateActor(actorDesc);
+			}
+		}
+
+		private void CreateBox()
+		{
+			var random = new Random();
+
+			int w = random.Next(1, 8);
+			int h = random.Next(1, 8);
+			int d = random.Next(1, 8);
+
+			int x = random.Next(-10, 10);
+			int y = random.Next(-10, 10);
+
+			var desc = new ActorDescription(new BoxShapeDescription(w, h, d))
+			{
+				BodyDescription = new BodyDescription(100),
+				GlobalPose =
+					Matrix.RotationY((float)(random.NextDouble() * 2 * Math.PI)) *
+					Matrix.Translation(x, 50, y)
+			};
+
+			var actor = Engine.Scene.CreateActor(desc);
 		}
 
 		private void ProcessKeyboard()
