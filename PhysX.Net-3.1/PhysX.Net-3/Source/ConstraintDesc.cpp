eff168da65430a36ad21dd6c6d21531f15ad74de
Chad Mellor <chad@stilldesign.co.nz>
2012-06-26 22:18:26 +0000

    Removed 3.1

--- PhysX.Net-3.1/PhysX.Net-3/Source/ConstraintDesc.cpp
+++
@@ -1,7 +0,0 @@
-#include "StdAfx.h"
-#include "ConstraintDesc.h"
-
-ConstraintDesc::ConstraintDesc()
-{
-	
-}
--- PhysX.Net-3.1/PhysX.Net-3/Source/SceneSweepOperationObject.cpp
+++
@@ -1,9 +0,0 @@
-#include "StdAfx.h"
-#include "SceneSweepOperationObject.h"
-
-SceneSweepOperationObject::SceneSweepOperationObject(PhysX::Geometry^ geometry, Matrix pose, [Optional] Nullable<PhysX::FilterData> filterData)
-{
-	this->Geometry = Geometry;
-	this->Pose = pose;
-	this->FilterData = filterData;
-}
