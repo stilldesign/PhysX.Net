68b9f943bfa6b3fc6427b30c392a1bab25e8f34e
Chad Mellor <chad@stilldesign.co.nz>
2012-08-11 08:26:52 +0000

    Added basic sample of each joint type

--- PhysX.Net-3.2/Samples/JointsSample/JointsSample.cs
+++ PhysX.Net-3.2/Samples/JointsSample/JointsSample.cs
@@ -23,33 +23,111 @@ namespace PhysX.Samples.JointsSample
 
 			// Spherical Joint
 			{
-				// Actor 0
-				var rigidActor0 = scene.Physics.CreateRigidDynamic();
-				rigidActor0.CreateShape(new BoxGeometry(2, 2, 2), material);
-
-				rigidActor0.GlobalPose = Matrix.Translation(0, 10, 0);
-				rigidActor0.SetMassAndUpdateInertia(10);
-
-
-
-				// Actor 1
-				//var rigidActor1 = scene.Physics.CreateRigidDynamic();
-				//rigidActor1.CreateShape(new BoxGeometry(2, 2, 2), material);
-
-				//rigidActor1.GlobalPose = Matrix.Translation(-20, 10, 0);
-				//rigidActor1.SetMassAndUpdateInertia(10);
-
-				//rigidActor1.Flags = RigidDynamicFlags.Kinematic; // Pin this actor in space
-
-				//scene.AddActor(rigidActor1);
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(0, 10, 0);
+				box.SetMassAndUpdateInertia(10);
 
 				//
 
-				var sphericalJoint = scene.CreateJoint<SphericalJoint>(rigidActor0, Matrix.Translation(0, 4, 0), null, Matrix.Translation(0, 15, 0));
-
+				var sphericalJoint = scene.CreateJoint<SphericalJoint>(box, Matrix.Translation(0, 4, 0), null, Matrix.Translation(0, 15, 0));
 				sphericalJoint.ConstraintFlag = ConstraintFlag.Visualization;
 
-				scene.AddActor(rigidActor0);
+				scene.AddActor(box);
+			}
+
+			// Revolute Joint
+			{
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(-10, 10, 0);
+				box.SetMassAndUpdateInertia(10);
+
+				//
+
+				var revoluteJoint = scene.CreateJoint<RevoluteJoint>(box, Matrix.Translation(0, 4, 0), null, Matrix.Translation(-10, 15, 0));
+				revoluteJoint.ConstraintFlag = ConstraintFlag.Visualization;
+
+				scene.AddActor(box);
+			}
+
+			// Breakable Revolute Joint
+			{
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(10, 10, 0);
+				box.SetMassAndUpdateInertia(10);
+
+				//
+
+				var revoluteJoint = scene.CreateJoint<RevoluteJoint>(box, Matrix.Translation(0, 4, 0), null, Matrix.Translation(10, 15, 0));
+				revoluteJoint.ConstraintFlag = ConstraintFlag.Visualization;
+				revoluteJoint.BreakForce = 0.001f;
+
+				scene.AddActor(box);
+			}
+
+			// Distance Joint
+			{
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(-20, 10, 0);
+				box.SetMassAndUpdateInertia(10);
+
+				//
+
+				var distanceJoint = scene.CreateJoint<DistanceJoint>(box, Matrix.Translation(0, 4, 0), null, Matrix.Translation(-20, 15, 0));
+				distanceJoint.ConstraintFlag = ConstraintFlag.Visualization;
+				distanceJoint.MinimumDistance = 1;
+				distanceJoint.MaximumDistance = 3;
+
+				scene.AddActor(box);
+			}
+
+			// Fixed Joint
+			{
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(-30, 15, 0);
+				box.SetMassAndUpdateInertia(10);
+
+				//
+
+				var fixedJoint = scene.CreateJoint<FixedJoint>(box, Matrix.Identity, null, Matrix.Translation(-30, 15, 0));
+				fixedJoint.ConstraintFlag = ConstraintFlag.Visualization;
+
+				scene.AddActor(box);
+			}
+
+			// Prismatic Joint
+			{
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(-40, 15, 0);
+				box.SetMassAndUpdateInertia(10);
+
+				//
+
+				var prismaticJoint = scene.CreateJoint<PrismaticJoint>(box, Matrix.Identity, null, Matrix.Translation(-40, 15, 0));
+				prismaticJoint.ConstraintFlag = ConstraintFlag.Visualization;
+
+				scene.AddActor(box);
+			}
+
+			// D6 Joint
+			{
+				var box = scene.Physics.CreateRigidDynamic();
+				box.CreateShape(new BoxGeometry(2, 2, 2), material);
+				box.GlobalPose = Matrix.Translation(20, 15, 0);
+				box.SetMassAndUpdateInertia(10);
+
+				//
+
+				var d6Joint = scene.CreateJoint<D6Joint>(box, Matrix.Identity, null, Matrix.Translation(20, 15, 0));
+				d6Joint.ConstraintFlag = ConstraintFlag.Visualization;
+				d6Joint.SetMotion(D6Axis.Twist, D6Motion.Free); // Spin on the X axis
+
+				scene.AddActor(box);
 			}
 		}
 
