aa70ed026f7443c0c9d9b38d920252d4806ee10b
Chad Mellor <chad@stilldesign.co.nz>
2014-04-20 23:00:28 +1200

    Cleaned

--- PhysX.Net-3.3/PhysX.Net-3/Source/VehicleWheelQueryResult.cpp
+++ PhysX.Net-3.3/PhysX.Net-3/Source/VehicleWheelQueryResult.cpp
@@ -5,21 +5,21 @@ VehicleWheelQueryResult^ VehicleWheelQueryResult::ToManaged(PxWheelQueryResult* 
 {
 	auto managed = gcnew VehicleWheelQueryResult();
 
-	managed->SuspensionLineStart = MathUtil::PxVec3ToVector3(unmanaged->suspLineStart);
-	managed->SuspensionLineDirection = MathUtil::PxVec3ToVector3(unmanaged->suspLineDir);
+	managed->SuspensionLineStart = MV(unmanaged->suspLineStart);
+	managed->SuspensionLineDirection = MV(unmanaged->suspLineDir);
 	managed->SuspensionLineLength = unmanaged->suspLineLength;
 	managed->IsInAir = unmanaged->isInAir;
 	managed->TireContactActor = unmanaged->tireContactActor == NULL ? nullptr : ObjectTable::GetObject<Actor^>((intptr_t)unmanaged->tireContactActor);
 	managed->TireContactShape = unmanaged->tireContactShape == NULL ? nullptr : ObjectTable::GetObject<Shape^>((intptr_t)unmanaged->tireContactShape);
 	managed->TireSurfaceMaterial = unmanaged->tireSurfaceMaterial == NULL ? nullptr : ObjectTable::GetObject<Material^>((intptr_t)unmanaged->tireSurfaceMaterial);
 	managed->TireSurfaceType = unmanaged->tireSurfaceType;
-	managed->TireContactPoint = MathUtil::PxVec3ToVector3(unmanaged->tireContactPoint);
-	managed->TireContactNormal = MathUtil::PxVec3ToVector3(unmanaged->tireContactNormal);
+	managed->TireContactPoint = MV(unmanaged->tireContactPoint);
+	managed->TireContactNormal = MV(unmanaged->tireContactNormal);
 	managed->TireFriction = unmanaged->tireFriction;
 	managed->SuspensionJounce = unmanaged->suspJounce;
 	managed->SuspensionSpringForce = unmanaged->suspSpringForce;
-	managed->TireLongitudinalDirection = MathUtil::PxVec3ToVector3(unmanaged->tireLongitudinalDir);
-	managed->TireLateralDirection = MathUtil::PxVec3ToVector3(unmanaged->tireLateralDir);
+	managed->TireLongitudinalDirection = MV(unmanaged->tireLongitudinalDir);
+	managed->TireLateralDirection = MV(unmanaged->tireLateralDir);
 	managed->LongitudinalSlip = unmanaged->longitudinalSlip;
 	managed->LateralSlip = unmanaged->lateralSlip;
 	managed->SteerAngle = unmanaged->steerAngle;
