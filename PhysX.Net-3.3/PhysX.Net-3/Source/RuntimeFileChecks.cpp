c07d36cb30a2fa680edfc6b5e5ccf6074dad4130
Chad Mellor <chad@stilldesign.co.nz>
2015-05-24 21:33:32 +0100

    Specified x64 DLL files to check Set the linker to take the CHECKED.lib files - as a result, use the Multithreaded DLL C++ runtime (not the static link nor the debug version)

--- PhysX.Net-3.3/PhysX.Net-3/Source/RuntimeFileChecks.cpp
+++ PhysX.Net-3.3/PhysX.Net-3/Source/RuntimeFileChecks.cpp
@@ -12,10 +12,19 @@ using namespace System::Security::Cryptography;
 void RuntimeFileChecks::Check()
 {
 #if WIN64
-	CheckFile("PhysX3CHECKED_x64.dll", PhysXDllVersion);
-	CheckFile("PhysX3CommonCHECKED_x64.dll", PhysXDllVersion);
-	CheckFile("PhysX3CookingCHECKED_x64.dll", PhysXDllVersion);
-	CheckFile("PhysX3GpuCHECKED_x64.dll", PhysXDllVersion);
+	#if PHYSX_CHECKED
+		CheckFile("PhysX3CHECKED_x64.dll", PhysXDllVersion);
+		CheckFile("PhysX3CommonCHECKED_x64.dll", PhysXDllVersion);
+		CheckFile("PhysX3CookingCHECKED_x64.dll", PhysXDllVersion);
+		CheckFile("PhysX3GpuCHECKED_x64.dll", PhysXDllVersion);
+	#elif PHYSX_RELEASE
+		CheckFile("PhysX3_x64.dll", PhysXDllVersion);
+		CheckFile("PhysX3Common_x64.dll", PhysXDllVersion);
+		CheckFile("PhysX3Cooking_x64.dll", PhysXDllVersion);
+		CheckFile("PhysX3Gpu_x64.dll", PhysXDllVersion);
+	#else
+		#error PhysX Build Configuration not specified for x64
+	#endif
 #elif WIN32
 	#if PHYSX_CHECKED
 		CheckFile("PhysX3CHECKED_x86.dll", PhysXDllVersion);
