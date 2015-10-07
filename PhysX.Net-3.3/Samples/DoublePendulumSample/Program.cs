90d9c4124a509bad67971dfe0392fbd32c059640
Chad Mellor <chad@stilldesign.co.nz>
2015-10-07 06:27:55 +0100

    Simple example of a double pendulum

---
+++ PhysX.Net-3.3/Samples/DoublePendulumSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublePendulumSample
+{
+	public class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			new DoublePendulumSample();
+		}
+	}
+}
