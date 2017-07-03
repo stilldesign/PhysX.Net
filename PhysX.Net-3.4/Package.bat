1c3eb048c966f7cb61c9464320542fd75b891ea3
Chad Mellor <chad@stilldesign.co.nz>
2017-07-03 13:21:31 +1200

    Updated version numbers, references and nuspec dependency files

--- PhysX.Net-3.4/Package.bat
+++ PhysX.Net-3.4/Package.bat
@@ -1,5 +1,5 @@
 
-cd C:\Development\PhysX.Net\PhysX.Net-3.3
+cd F:\PhysX.Net\PhysX.Net-3.4
 C:
 
 md Package
@@ -7,31 +7,31 @@ cd Package
 rd Working /S /Q
 md Working
 cd Working
-md "PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4"
-cd "PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4"
+md "PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0"
+cd "PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0"
 md "Assemblies"
 md "Assemblies\x64"
 md "Assemblies\x64\Checked"
 md "Assemblies\x64\Release"
 cd ..\..\..
 
-msbuild PhysX.Net-3.3.4.sln /p:Configuration=Checked;Platform=x64 /t:Clean,Build
-msbuild PhysX.Net-3.3.4.sln /p:Configuration=Release;Platform=x64 /t:Clean,Build
+msbuild PhysX.Net-3.4.0.sln /p:Configuration=Checked;Platform=x64 /t:Clean,Build
+msbuild PhysX.Net-3.4.0.sln /p:Configuration=Release;Platform=x64 /t:Clean,Build
 
 # Samples
-md "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Samples"
-robocopy Samples "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Samples" /E /XF *.suo *.vssscc *.cachefile *.user *.vspscc *.pdb *.vshost.exe* /XD obj Release
-md "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Assemblies"
-robocopy Assemblies "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Assemblies" /E
+md "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Samples"
+robocopy Samples "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Samples" /E /XF *.suo *.vssscc *.cachefile *.user *.vspscc *.pdb *.vshost.exe* /XD obj Release
+md "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Assemblies"
+robocopy Assemblies "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Assemblies" /E
 
-xcopy "PhysX.Net-3\bin\x64\Checked\PhysX.Net.dll" "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Assemblies\x64\Checked"
-xcopy "PhysX.Net-3\bin\x64\Checked\PhysX.Net.pdb" "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Assemblies\x64\Checked"
-xcopy "PhysX.Net-3\bin\x64\Release\PhysX.Net.dll" "Package\Working\PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4\Assemblies\x64\Release"
+xcopy "PhysX.Net-3\bin\x64\Checked\PhysX.Net.dll" "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Assemblies\x64\Checked"
+xcopy "PhysX.Net-3\bin\x64\Checked\PhysX.Net.pdb" "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Assemblies\x64\Checked"
+xcopy "PhysX.Net-3\bin\x64\Release\PhysX.Net.dll" "Package\Working\PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0\Assemblies\x64\Release"
 
 cd "Package\Working"
 
 attrib * -R /S /D
 
-..\..\..\Tools\7-Zip\7z.exe a "PhysX.Net 0.5.1 for NVIDIA PhysX 3.3.4.zip" .
+..\..\..\Tools\7-Zip\7z.exe a "PhysX.Net 0.7.0 for NVIDIA PhysX 3.4.0.zip" .
 
 cd ..\..
