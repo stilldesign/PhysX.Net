4eb730e93b5e9883c79e92c9c88771e22900a81c
Chad Mellor <chad@stilldesign.co.nz>
2017-07-03 14:22:23 +1200

    Used code

--- PhysX.Net-3.4/Samples/Engine/Math/Math.cs
+++ PhysX.Net-3.4/Samples/Engine/Math/Math.cs
@@ -6,16 +6,6 @@ namespace PhysX.Samples
 {
 	public static class Mathematics
 	{
-		public static float Clamp(float value, float max, float min)
-		{
-			if (value < min)
-				return min;
-			if (value > max)
-				return max;
-
-			return value;
-		}
-
 		public static Vector3 DirectionFromViewMatrix(Matrix viewMatrix)
 		{
 			return Vector3.Normalize(new Vector3(viewMatrix.M13, viewMatrix.M23, viewMatrix.M33));
