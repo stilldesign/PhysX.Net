4c743dfc904a32c4b67455c956919e103bab54b2
Chad Mellor <chad@stilldesign.co.nz>
2018-03-09 16:59:35 +1300

    Bumped the version number to 0.11.0

--- PhysX.Net-3.4/PhysX.Net-3/Source/AssemblyInfo.cpp
+++ PhysX.Net-3.4/PhysX.Net-3/Source/AssemblyInfo.cpp
@@ -11,12 +11,12 @@ using namespace System::Security::Permissions;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly:AssemblyTitleAttribute("PhysX.Net for NVIDIA PhysX 3.4")];
-[assembly:AssemblyDescriptionAttribute(".NET wrapper for NVIDIA PhysX 3.4")];
+[assembly:AssemblyTitleAttribute("PhysX.Net for NVIDIA PhysX 3.4.1")];
+[assembly:AssemblyDescriptionAttribute(".NET wrapper for NVIDIA PhysX 3.4.1")];
 [assembly:AssemblyConfigurationAttribute("")];
 [assembly:AssemblyCompanyAttribute("StillDesign")];
 [assembly:AssemblyProductAttribute("PhysX.Net")];
-[assembly:AssemblyCopyrightAttribute("Copyright (c) StillDesign 2017")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) StillDesign 2018")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
@@ -32,7 +32,7 @@ using namespace System::Security::Permissions;
 // by using the '*' as shown below:
 
 // Versioning - http://semver.org/
-[assembly:AssemblyVersionAttribute("0.10.0")];
+[assembly:AssemblyVersionAttribute("0.11.0")];
 
 [assembly:ComVisible(false)];
 
