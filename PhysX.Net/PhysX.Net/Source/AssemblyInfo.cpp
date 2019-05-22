a2e7fccfe65652256eafa4050090f02555f0d480
Chad Mellor <chad@stilldesign.co.nz>
2019-05-22 22:37:44 +1200

    Bumped major version because of the PhysX SDK update

--- PhysX.Net/PhysX.Net/Source/AssemblyInfo.cpp
+++ PhysX.Net/PhysX.Net/Source/AssemblyInfo.cpp
@@ -32,7 +32,7 @@ using namespace System::Security::Permissions;
 // by using the '*' as shown below:
 
 // Versioning - http://semver.org/
-[assembly:AssemblyVersionAttribute("0.1.0")];
+[assembly:AssemblyVersionAttribute("1.0.0")];
 
 [assembly:ComVisible(false)];
 
